aa94f98036480c6a0cc0204018f368561595d878
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-16 13:26:48 -0400

    Refactor to respond to invalid inputs

--- super_fizz_buzz_runner.rb
+++ super_fizz_buzz_runner.rb
@@ -4,11 +4,21 @@ super_fizz = SuperFizzBuzz.new
 
 puts "Welcome to SuperFizzBuzz! Enter a number or a range..."
 
+puts
+
 input = gets.chomp
 
-if input.include?(",")
+puts
+
+if input.match?(/[[:alpha:]]/)
+  puts "Your input needs to be a single number or in the format first_number,second_number"
+elsif input.include?(",")
   input_array = input.split(",")
-  puts super_fizz.output_range(input_array[0].to_i,input_array[1].to_i)
+  if input_array[0].to_i > input_array[1].to_i
+    puts "Your first number should be lower than your second number please."
+  else
+    puts super_fizz.output_range(input_array[0].to_i,input_array[1].to_i)
+  end
 else
   puts super_fizz.output(input.to_i)
 end
